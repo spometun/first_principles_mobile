0463ebc14419a95363aafb49b69e7e1db253eca0
Sergiy Pometun <s.pometun@samsung.com>
2017-06-21 06:32:52 +0300

    finish Ukrainian translation + fix Swedish flag

--- www/js/refresh-button.js
+++ www/js/refresh-button.js
@@ -1,15 +1,32 @@
 window.addEventListener('load',  () => {
+  const OK_RESPONSE = 'ok';
+  const TOO_LONG_ERROR = 'Looks like something went wrong. Update timeout. Try to reload page (press F5)';
+  const REFRESH_BUTTON_TEXT = 'Refreshing translation';
+  const REFRESH_BUTTON_REFRESHING_TEXT = 'Refreshing...';
+
   const updateContentURL = '../../../api/update_translations.php?lang=' + CUR_LANGUAGE.language;
 
-  $(document).ready(() => $('#refresh-button').closest('.ui-btn').show());
+  const changeButtonText = (button, text) => (
+    button.siblings('.ui-btn-inner').find('.ui-btn-text').text(text)
+  );
 
   const { language } = CUR_LANGUAGE;
   const refreshButton = $('#refresh-button');
   refreshButton.bind('click', () => {
     fetch(updateContentURL).then(response =>
-      response.blob().then(() => location.reload())
+      response.text().then(text => {
+        console.log(text);
+        switch (text) {
+          case OK_RESPONSE: {
+            location.reload();
+          } break;
+          case TOO_LONG_ERROR: {
+            changeButtonText(refreshButton, REFRESH_BUTTON_TEXT);
+            alert(text);
+          } break;
+        }
+      })
     );
-    refreshButton.siblings('.ui-btn-inner')
-      .find('.ui-btn-text').text("Refreshing...");
+    changeButtonText(refreshButton, REFRESH_BUTTON_REFRESHING_TEXT);
   });
 });
